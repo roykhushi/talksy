55aa73b73fa77f992164c213e4b17a68a552390b
Khushi Roy <roykhushi432@gmail.com>
2025-02-04 23:10:09 +0530

    started w frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 dotenv.config();
 
 const app = express();
@@ -11,6 +12,10 @@ const PORT = process.env.PORT;
 
 app.use(express.json()); //it will extract the contents of the body in json format
 app.use(cookieParser()); //parsing the cookie to extract the jwt
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
 
 //building routes
 
---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,26 @@
+import {create} from "zustand";
+import { axiosInstance } from "../lib/axios";
+
+export const useAuthStore = create((set) => ({
+    //checking if the user is authenticated or not for eg upon refreshing
+    authUser : null,
+    //loading states
+    isSigningUp: false,
+    isLoggedIn: false,
+    isUpdatingProfile: false,
+    isCheckingAuth: true,
+
+    checkAuth: async() =>{
+        try {
+            const res = await axiosInstance.get("/auth/check");
+            set({authUser: res.data});
+        } catch (error) {
+            console.log(`error in zustand store for checking auth ${error.message}`);
+            set({authUser:null});
+        }
+        finally{
+            set({isCheckingAuth: false});
+        }
+    }
+
+}));
